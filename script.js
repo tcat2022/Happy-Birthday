b9926e6dd38f97d3c9e88d2d099f2eceacc62975
tommy <tcatty7@gmail.com>
2023-12-01 14:31:00 -0500

    forgot to commit lol

---
+++ script.js
@@ -0,0 +1,12 @@
+let card = document.getElementById('card');
+let cursive2 = document.getElementById('cursive2');
+let cursive5 = document.getElementById('cursive5');
+let cursive4 = document.getElementById('cursive4');
+let cursive3 = document.getElementById('cursive3');
+
+card.addEventListener('mouseover', function() {
+cursive2.classList.add('cursive2');
+cursive5.classList.add('cursive5');
+cursive4.classList.add('cursive4');
+cursive3.classList.add('cursive3');
+  });
