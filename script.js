f53faed4521a9af212b7c744b63281bd1cbbe6e1
tommy <tcatty7@gmail.com>
2023-12-04 20:06:16 -0500

    testing look on tablet (shrugs)

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ let h1 = document.getElementById('h1');
 let h3 = document.getElementById('h3');
 let h2 = document.getElementById('h2');
 
-card.addEventListener('mouseover', function() {
-h1.classList.add('h1');
-h2.classList.add('h2');
-h3.classList.add('h3');
-appy.classList.add('appy');
-  });
+card.addEventListener('mouseover', function () {
+  h1.classList.add('h1');
+  h2.classList.add('h2');
+  h3.classList.add('h3');
+  appy.classList.add('appy');
+
+  setTimeout(function () {
+    card.style.cursor = 'url(light.png) ,pointer'
+  }, 450);
+
+});
