08dd5ad3efbced8ac7bafda5a7cd742c4e511e5b
tommy <tcatty7@gmail.com>
2023-12-19 15:10:30 -0500

    finished candles

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let t = document.getElementById('t');
 let jack1 = document.getElementById('jack1');
 let body = document.getElementById('body');
 let canvas = document.getElementById('canvas');
+let flame1 = document.getElementById('flame1');
+let flame2 = document.getElementById('flame2');
+let stem1 = document.getElementById('stem1');
+let stem2 = document.getElementById('stem2');
 
 card.addEventListener('mouseover', function () {
   h1.classList.add('h1');
@@ -22,7 +26,7 @@ card.addEventListener('mouseover', function () {
  let a = setTimeout(function(){
     canvas.style.display = 'none';
   },600)
-  card.style.cursor = 'url(light.png) ,pointer'
+ card.style.cursor = 'url(light.png) , auto'
   card.addEventListener('mouseleave', () => {
    setTimeout(function() {
     canvas.style.display = 'inherit'; 
@@ -31,19 +35,25 @@ card.addEventListener('mouseover', function () {
     })
 });
 
+stem1.addEventListener('click', function() {
+    flame1.style.display = 'inherit';
+})
+stem2.addEventListener('click', function() {
+    flame2.style.display = 'inherit';
+})
+
+
     const COLOR_SPACE = "black";
     const COLOR_STARS = "white";
-    const STAR_NUM = 500; // number of stars in the starfield
-    const STAR_SIZE = 0.003; // max star size as a fraction of screen width
-    const STAR_SPEED = 0.1; // fraction of screen width per second
+    const STAR_NUM = 500;
+    const STAR_SIZE = 0.003;
+    const STAR_SPEED = 0.1; 
     var ctx = canvas.getContext("2d");
     canvas.height = document.documentElement.clientHeight;
     canvas.width = document.documentElement.clientWidth;
-    // set up the stars
     var stars = [];
     var starSpeed = STAR_SPEED * canvas.width;
     var xv = starSpeed * randomSign() * Math.random();
-    // Using Pythagoras' theorem, yv = sqrt(starSpeed^2 - xv^2)
     var yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
     for (let i = 0; i < STAR_NUM; i++) {
         let speedMult = Math.random() * 1.5 + 0.5;
@@ -56,41 +66,34 @@ card.addEventListener('mouseover', function () {
         }
     }
     
-    // set up the animation loop
     var timeDelta, timeLast = 0;
     requestAnimationFrame(loop);
     
     function loop(timeNow) {
-    
-        // calculate the time difference
+
         timeDelta = timeNow - timeLast;
         timeLast = timeNow;
     
-        // space background
         ctx.fillStyle = COLOR_SPACE;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     
-        // draw the stars
         ctx.fillStyle = COLOR_STARS;
         for (let i = 0; i < STAR_NUM; i++) {
             ctx.beginPath();
             ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI * 2);
             ctx.fill();
     
-            // update the star's x position
+
             stars[i].x += stars[i].xv * timeDelta * 0.001;
-    
-            // reposition the star to the other side if it goes off screen
+
             if (stars[i].x < 0 - stars[i].r) {
                 stars[i].x = canvas.width + stars[i].r;
             } else if (stars[i].x > canvas.width + stars[i].r) {
                 stars[i].x = 0 - stars[i].r;
             }
             
-            // update the star's y position
             stars[i].y += stars[i].yv * timeDelta * 0.001;
     
-            // reposition the star to the other side if it goes off screen
             if (stars[i].y < 0 - stars[i].r) {
                 stars[i].y = canvas.height + stars[i].r;
             } else if (stars[i].y > canvas.height + stars[i].r) {
@@ -98,7 +101,6 @@ card.addEventListener('mouseover', function () {
             }
         }
     
-        // call the next frame
         requestAnimationFrame(loop);
     }
     
